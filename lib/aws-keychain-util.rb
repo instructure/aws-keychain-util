776920e89b98debb7985ee520dd0ec26807a21a3
Cody Cutrer <cody@cutrer.us>
2015-04-01 14:22:05 -0600

    improve handling of "default" prefs
    so that `shell` command doesn't crash if there isn't one

--- lib/aws-keychain-util.rb
+++ lib/aws-keychain-util.rb
@@ -2,16 +2,15 @@ module AwsKeychainUtil
   PREFS_FILE = File.expand_path "~/.aws-keychain-util"
 
   def self.load_keychain
-    keychain = if File.exist? PREFS_FILE
-      prefs = self.prefs
-      Keychain.open(prefs['aws_keychain_name'])
-    else
-      Keychain.default
-    end
-    keychain
+    name = prefs['aws_keychain_name']
+    name ? Keychain.open(name) : Keychain.default
   end
 
   def self.prefs
-    JSON.parse(File.read(PREFS_FILE))
+    if File.exist? PREFS_FILE
+      JSON.parse(File.read(PREFS_FILE))
+    else
+      {}
+    end
   end
 end
